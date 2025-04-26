cfce3dff39ffb26bd480d9356dfbff3ead501a98
Parralax <136958467+Parralax128@users.noreply.github.com>
2025-04-27 00:01:02 +0700

    Delete Source/LeniencyHelperModuleSaveData.cs

--- Source/LeniencyHelperModuleSaveData.cs
+++
@@ -1,9 +0,0 @@
-using static Celeste.Mod.LeniencyHelper.Tweaks.DirectionalReleaseProtection;
-
-namespace Celeste.Mod.LeniencyHelper;
-
-public class LeniencyHelperModuleSaveData : EverestModuleSaveData {
-
-
-
-}
