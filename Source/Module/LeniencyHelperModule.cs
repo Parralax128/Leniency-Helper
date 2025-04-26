621d45b4cceaf7b1004a24aba4501d864d158b57
Parralax <136958467+Parralax128@users.noreply.github.com>
2025-04-27 00:02:04 +0700

    Add files via upload

---
+++ Source/Module/LeniencyHelperModule.cs
@@ -0,0 +1,202 @@
+﻿using Celeste.Mod.LeniencyHelper.CrossModSupport;
+using Celeste.Mod.LeniencyHelper.Tweaks;
+using MonoMod.ModInterop;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Celeste.Mod.LeniencyHelper.UI;
+using System.Linq;
+using System.Reflection;
+
+namespace Celeste.Mod.LeniencyHelper.Module;
+public class LeniencyHelperModule : EverestModule
+{
+    #region very important generated stuff
+
+    public static LeniencyHelperModule Instance { get; private set; }
+
+    public override Type SettingsType => typeof(LeniencyHelperModuleSettings);
+    public static LeniencyHelperModuleSettings Settings => (LeniencyHelperModuleSettings)Instance._Settings;
+
+    public override Type SessionType => typeof(LeniencyHelperModuleSession);
+    public static LeniencyHelperModuleSession Session => (LeniencyHelperModuleSession)Instance._Session;
+
+    public override Type SaveDataType => typeof(LeniencyHelperModuleSaveData);
+    public static LeniencyHelperModuleSaveData SaveData => (LeniencyHelperModuleSaveData)Instance._SaveData;
+    #endregion
+
+    public static Dictionary<(string, Version), bool> ModsLoaded = new Dictionary<(string, Version), bool>
+    {
+        { ("MaxHelpingHand", new Version(1,30,0)), false },
+        { ("ShroomHelper", new Version(1,0,0)), false },
+        { ("VivHelper", new Version(1,12,3)), false },
+        { ("ExtendedVariantMode", new Version(0,35,0)), false }
+    };
+    public enum Inputs
+    {
+        Jump,
+        Dash,
+        Demo
+    }
+    public enum Dirs
+    {
+        Up, Down,
+        Left, Right,
+        All, None
+    }
+
+    public static string[] TweakList =
+    {
+        "AutoSlowfall",
+        "BackwardsRetention",
+        "BufferableClimbtrigger",
+        "BufferableExtends",
+        "ConsistentDashOnDBlockExit",
+        "CornerWaveLeniency",
+        "CustomBufferTime",
+        "CustomDashbounceTiming",
+        "DashCDIgnoreFFrames",
+        "DirectionalReleaseProtection",
+        "DisableBackboost",
+        "DisableForceMove",
+        "DynamicCornerCorrection",
+        "DynamicWallLeniency",
+        "ExtendBufferOnFreezeAndPickup",
+        "ForceCrouchDemodash",
+        "IceWallIncreaseWallLeniency",
+        "InstantAcceleratedJumps",
+        "InstantClimbHop",
+        "NoFailedTech",
+        "RefillDashInCoyote",
+        "RemoveDBlockCCorection",
+        "RetainSpeedCornerboost",
+        "SolidBlockboostProtection",
+        "SuperOverWalljump",
+        "WallAttraction",
+        "WallCoyoteFrames"
+    };
+
+    public static SettingList DefaultSettings = new SettingList();
+
+    public LeniencyHelperModule()
+    {
+        Instance = this;
+        Logger.SetLogLevel(nameof(LeniencyHelperModule), LogLevel.Verbose);
+
+        foreach (FieldInfo field in typeof(SettingList).GetFields())
+            SettingMaster.SettingListFields[field.Name] = field;
+    }
+    public static void Log(object input)
+    {
+        Logger.Log(LogLevel.Info, "LeniencyHelper", input == null ? "null" : input.ToString());
+    }
+
+    public static bool CollideOnWJdist<T>(Monocle.Entity entity, int dir, Vector2? at) where T : Monocle.Entity
+    {
+        Vector2 savePos = entity.Position;
+        if (at.HasValue) entity.Position = at.Value;
+        Rectangle checkRect = entity.Collider.Bounds;
+        if (at.HasValue) entity.Position = savePos;
+
+        checkRect.Width += dir == 1 ? Session.wjDistR : Session.wjDistL;
+        checkRect.X += (dir == 1 ? Session.wjDistR : Session.wjDistL) * dir;
+
+        return entity.Scene.CollideCheck<T>(checkRect);
+    }
+    public static bool CollideOnWJdist(Monocle.Entity entity, Monocle.Entity with, int dir, Vector2? at)
+    {
+        Vector2 savePos = entity.Position;
+        if (at.HasValue) entity.Position = at.Value;
+        Rectangle checkRect = entity.Collider.Bounds;
+        if (at.HasValue) entity.Position = savePos;
+
+        checkRect.Width += dir == 1 ? Session.wjDistR : Session.wjDistL;
+        checkRect.X += (dir == 1 ? Session.wjDistR : Session.wjDistL) * dir;
+
+        return entity.Scene.CollideCheck(checkRect, with);
+    }
+    public override void Initialize()
+    {
+        base.Initialize();
+        foreach ((string, Version) mod in ModsLoaded.Keys)
+        {
+            ModsLoaded[mod] = Everest.Loader.DependencyLoaded(new EverestModuleMetadata { Name = mod.Item1, Version = mod.Item2 });
+        }
+        if (ModsLoaded[("MaxHelpingHand", new Version(1, 30, 0))])
+        {
+            SolidBlockboostProtection.LoadSidewaysHook();
+        }
+        if (ModsLoaded[("VivHelper", new Version(1, 12, 3))])
+        {
+            BufferableClimbtrigger.LoadVivHelperHooks();
+        }
+    }
+
+    public override void Load()
+    {
+        var loadHooksMethods = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypesSafe()).Where(x => x.IsClass)
+        .SelectMany(x => x.GetMethods()).Where(x => x.GetCustomAttributes(typeof(OnLoad), false).FirstOrDefault() != null);
+
+        foreach (MethodInfo method in loadHooksMethods)
+        {
+            method.Invoke(null, null);
+        }
+
+        Everest.Events.Level.OnCreatePauseMenuButtons += AddTweaksMenuButton;
+        Everest.Events.LevelLoader.OnLoadingThread += AddStamp;
+
+        typeof(GravityHelperImports).ModInterop();
+        typeof(ExtendedVariantImports).ModInterop();
+    }
+    public override void Unload()
+    {
+        var loadHooksMethods = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypesSafe()).Where(x => x.IsClass)
+        .SelectMany(x => x.GetMethods()).Where(x => x.GetCustomAttributes(typeof(OnUnload), false).FirstOrDefault() != null);
+
+        foreach (MethodInfo method in loadHooksMethods)
+        {
+            method.Invoke(null, null);
+        }
+
+        Everest.Events.Level.OnCreatePauseMenuButtons -= AddTweaksMenuButton;
+        Everest.Events.LevelLoader.OnLoadingThread -= AddStamp;
+    }
+
+
+    public override byte[] SerializeSession(int index)
+    {
+        return null;
+    }
+    public override void WriteSession(int index, byte[] data)
+    {
+        SessionSerialization.SaveSession(index, Session);
+    }
+    public override byte[] ReadSession(int index)
+    {
+        return null;
+    }
+    public override void DeserializeSession(int index, byte[] data)
+    {
+        _Session = SessionSerialization.LoadSession(index);
+    }
+
+
+    private void AddTweaksMenuButton(Level level, TextMenu menu, bool minimal)
+    {
+        if (!Settings.showSettings) return;
+
+        int optionsIndex = menu.Items.FindIndex(item =>
+            item.GetType() == typeof(TextMenu.Button) &&
+            ((TextMenu.Button)item).Label == Dialog.Clean("MENU_PAUSE_SAVEQUIT"));
+
+        if (optionsIndex == -1) return;
+
+        TextMenu.Button button = MenuButtonManager.BuildMenuButton(menu);
+        menu.Insert(optionsIndex, button);
+    }
+
+    private static void AddStamp(Level level)
+    {
+        level.Add(new StampRenderer(new Vector2(41f, 240f)));
+    }
+}
---
+++ Source/Module/LeniencyHelperModuleSession.cs
@@ -0,0 +1,91 @@
+using Microsoft.Xna.Framework;
+using Celeste.Mod.LeniencyHelper.Components;
+using System;
+using static Celeste.Mod.LeniencyHelper.Module.LeniencyHelperModule;
+using static Celeste.Mod.LeniencyHelper.Triggers.InputRequiresBlockboostTrigger;
+using System.Collections.Generic;
+using System.Linq;
+using YamlDotNet.Serialization;
+
+namespace Celeste.Mod.LeniencyHelper.Module;
+
+public class LeniencyHelperModuleSession : EverestModuleSession
+{
+
+    public LeniencyHelperModuleSession()
+    {
+        TriggerTweaks = TweakList.ToDictionary(tweak => tweak, tweak => false);
+        ControllerTweaks = TweakList.ToDictionary(tweak => tweak, tweak => false);
+        UseController = TweakList.ToDictionary(tweak => tweak, tweak => false);
+
+        TriggerSettings = new SettingList();
+        ControllerSettings = new SettingList();
+    }
+
+    public Dictionary<string, bool> TriggerTweaks { get; set; } = TweakList.ToDictionary(tweak => tweak, tweak => false);
+    public Dictionary<string, bool> ControllerTweaks { get; set; } = TweakList.ToDictionary(tweak => tweak, tweak => false);
+    public Dictionary<string, bool> UseController { get; set; } = TweakList.ToDictionary(tweak => tweak, tweak => false);
+
+    public SettingList TriggerSettings { get; set; } = new SettingList();
+    public SettingList ControllerSettings { get; set; } = new SettingList();
+
+
+    #region temp vars
+    public int wjDistR { get; set; } = 3;
+    public int wjDistL { get; set; } = 3;
+
+
+    //BufferableExtends
+    public float dashTimer { get; set; } = 0f;
+    public bool dreamDashEnded { get; set; } = false;
+
+    //CustomDashbounceTiming
+    public float dashbounceTimer { get; set; } = 0f;
+
+    //DashCDIgnoreFFrames
+    public Player modifiedPlayer { get; set; }
+    public bool useOrigFreeze { get; set; } = false;
+
+    //DynamicCornerCorrection
+    public Vector2 cornerCorrection { get; set; } = Vector2.Zero;
+
+    //ExtendBufferOnFreezeAndPickup
+    public float pickupDelay { get; set; } = 0.016f;
+
+    //FFramesExtendBuffer
+    public float pickupTimeLeft { get; set; } = 0f;
+    public int prevFrameState { get; set; } = 0;
+    public bool jumpExtended { get; set; } = false;
+    public bool dashExtended { get; set; } = false;
+    public bool demoExtended { get; set; } = false;
+
+    //InstantClimbHop
+    public bool movedDown { get; set; } = false;
+    public Solid savedClimbHopSolid { get; set; }
+
+    //NoFailedTech
+    public bool dashCrouched { get; set; } = false;
+    public float protectedDashAttackTimer { get; set; } = 0f;
+
+    //RefillDashInCoyote
+    public bool artificialChecking { get; set; } = false;
+
+    //RetainSpeedCornerboost
+    public float retainCbSpeed { get; set; }
+    public float retainCbSpeedTimer { get; set; }
+
+    #endregion
+
+
+    #region triggers
+    //ClearBlockBoostTrigger
+    public bool clearBlockBoostActivated { get; set; } = false;
+
+    //DisalbeAirMovementTrigger
+    public bool airMovementDisabled { get; set; } = false;
+
+    //InputRequiresBlockboostTrigger
+    public BindInfo[] BindList = Array.Empty<BindInfo>();
+    public Vector2 playerLiftboost { get; set; } = Vector2.Zero;
+    #endregion
+}
