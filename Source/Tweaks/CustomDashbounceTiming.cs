8c6ceeacfbf49e8a93934041284a48627485a2a9
Parralax <136958467+Parralax128@users.noreply.github.com>
2025-05-01 19:52:53 +0700

    Add files via upload

--- Source/Tweaks/CustomDashbounceTiming.cs
+++ Source/Tweaks/CustomDashbounceTiming.cs
@@ -1,17 +1,18 @@
-﻿using Monocle;
+﻿using Celeste.Mod.LeniencyHelper.Module;
+using Monocle;
 using System;
-using static Celeste.Mod.LeniencyHelper.SettingMaster;
 
 namespace Celeste.Mod.LeniencyHelper.Tweaks;
 
-public static class CustomDashbounceTiming
+public class CustomDashbounceTiming : AbstractTweak
 {
     [OnLoad]
     public static void LoadHooks()
     {
         On.Celeste.Player.SuperWallJump += SetCustomTiming;
-        On.Celeste.Player.NormalBegin += CustomDashbounce;
+        On.Celeste.Player.NormalBegin += DashbounceIfCan;
         On.Celeste.Player.Update += UpdateTimer;
+        On.Celeste.Player.DashBegin += TimerCheck;
     }
     [OnUnload]
     public static void UnloadHooks() 
@@ -19,44 +20,49 @@ public static class CustomDashbounceTiming
         On.Celeste.Player.SuperWallJump -= SetCustomTiming;
     }
 
-    private static bool justWallbounced = false;
-    private static void UpdateTimer(On.Celeste.Player.orig_Update orig, Player self)
-    {
-        var s = LeniencyHelperModule.Session;
-        if (s.dashbounceTimer > 0f) s.dashbounceTimer -= Engine.DeltaTime;
-
-        orig(self);
-    }
-    private static void CustomDashbounce(On.Celeste.Player.orig_NormalBegin orig, Player self)
-    {
-        var s = LeniencyHelperModule.Session;
-        if (s.Tweaks["CustomDashbounceTiming"].Enabled && !justWallbounced)
-        {
-            if(s.dashbounceTimer > 0f)
-            {
-                self.varJumpTimer = Math.Max(self.varJumpTimer, 0.05f);
-                s.dashbounceTimer = 0f;
-            }
-        }
-        justWallbounced = false;
-
-        orig(self);
-    }
     private static void SetCustomTiming(On.Celeste.Player.orig_SuperWallJump orig, Player self, int dir)
     {
         orig(self, dir);
+        if (!Enabled("CustomDashbounceTiming")) return;
 
-        if (LeniencyHelperModule.Session.Tweaks["CustomDashbounceTiming"].Enabled)
+        LeniencyHelperModule.Session.dashbounceTimer = GetSetting<float>("dashbounceTiming") *
+            (GetSetting<bool>("countDashbounceTimingInFrames") ? Engine.DeltaTime : 1f); ;
+    }
+    private static void UpdateTimer(On.Celeste.Player.orig_Update orig, Player self)
+    {
+        if (LeniencyHelperModule.Session.dashbounceTimer > 0f)
         {
-            LeniencyHelperModule.Session.dashbounceTimer = 0.2f + (GetSetting<bool>("countDashbounceTimingInFrames") ?
-                GetSetting<float>("dashbounceTiming") / Engine.FPS : GetSetting<float>("dashbounceTiming"));
+            LeniencyHelperModule.Session.dashbounceTimer -= Engine.DeltaTime;
+        }
 
-            justWallbounced = true;
-             
-            if(LeniencyHelperModule.Session.dashbounceTimer < self.varJumpTimer)
-            {
-                self.varJumpTimer = LeniencyHelperModule.Session.dashbounceTimer;
-            }
+        orig(self);
+    }
+    private static void TimerCheck(On.Celeste.Player.orig_DashBegin orig, Player self)
+    {
+        orig(self);
+        if (LeniencyHelperModule.Session.dashbounceTimer > 0f)
+        {
+            LeniencyHelperModule.Session.canDashbounce = true;
+            LeniencyHelperModule.Session.dashbounceTimer = 0f;
+        }
+        else
+        {
+            LeniencyHelperModule.Session.canDashbounce = false;
         }
     }
+    private static void DashbounceIfCan(On.Celeste.Player.orig_NormalBegin orig, Player self)
+    {
+        if (LeniencyHelperModule.Session.canDashbounce)
+        {
+            self.varJumpTimer = Math.Max(self.varJumpTimer, 0.05f);
+            LeniencyHelperModule.Session.canDashbounce = false;
+        }
+        else
+        {
+            self.varJumpTimer = 0f;
+        }
+
+        orig(self);
+    }
+
 }   
