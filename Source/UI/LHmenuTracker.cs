1cf547e8b8d56d3207e8b0f3b9b5bcff711711ec
Parralax <parralax@internet.ru>
2025-05-08 23:52:09 +0700

    -

--- Source/UI/LHmenuTracker.cs
+++ Source/UI/LHmenuTracker.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Monocle;
-using Celeste;
-using static Celeste.Mod.LeniencyHelper.Module.LeniencyHelperModule;
-using FMOD;
-using Celeste.Mod.LeniencyHelper.Tweaks;
-using Microsoft.Xna.Framework.Content;
-using MonoMod.Utils;
+﻿using Monocle;
 
 namespace Celeste.Mod.LeniencyHelper.UI
 {
@@ -17,7 +6,7 @@ namespace Celeste.Mod.LeniencyHelper.UI
     {
         public LHmenuTracker(bool active = true, bool visible = false) : base(active, visible)
         {
-            //a/fasfassadfdafdfdsa
+            
         }
     }
 }
