2363f0607214252826c30cc45b207b457a544f2d
Parralax <parralax@internet.ru>
2025-05-08 23:56:57 +0700

    =

--- Source/Controllers/GenericController.cs
+++ Source/Controllers/GenericController.cs
@@ -4,7 +4,6 @@ using Monocle;
 namespace Celeste.Mod.LeniencyHelper.Controllers;
 public abstract class GenericController : Entity
 {
-    //
     public string stopFlag;
     public bool persistent;
     private TransitionListener transitionListener;
